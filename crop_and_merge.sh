eb63bd52fb1340a55d7b72eec1f75f90d8b1eef8
Őry Máté <orymate@ubuntu.com>
2012-06-02 14:26:43 +0200

    mktemp -d instead of mkdir ./tmp

--- crop_and_merge.sh
+++ crop_and_merge.sh
@@ -3,10 +3,12 @@ INPUT=$1
 PAGES=$(pdfinfo ${INPUT}.pdf | grep Pages | cut -d ":" -f 2 | sed 's/ //g')
 i=1
 params=""
-mkdir tmp
-pdfcrop --bbox '120 465 475 730' $INPUT.pdf ./tmp/${INPUT}_A.pdf
-pdfcrop --bbox '120 115 475 380' $INPUT.pdf ./tmp/${INPUT}_B.pdf
-cd tmp
+tmp=$(mktemp -d)
+trap "rm -r $tmp" EXIT
+
+pdfcrop --bbox '120 465 475 730' $INPUT.pdf $tmp/${INPUT}_A.pdf
+pdfcrop --bbox '120 115 475 380' $INPUT.pdf $tmp/${INPUT}_B.pdf
+cd $tmp
 while [ $i -lt $PAGES ]; do
     pdftk A=${INPUT}_A.pdf B=${INPUT}_B.pdf cat A$i B$i output ${INPUT}_part_${i}.pdf
     i=$[$i+1]
@@ -17,8 +19,7 @@ while [ $i -lt $PAGES ]; do
     i=$[$i+1]
 done
 pdftk $params cat output ${INPUT}_kindle.pdf
-cd ..
-mv ./tmp/${INPUT}_kindle.pdf ${INPUT}_tmp.pdf
+cd -
+mv $tmp/${INPUT}_kindle.pdf ${INPUT}_tmp.pdf
 pdftk ${INPUT}_tmp.pdf cat 1-endW output ${INPUT}_kindle.pdf
 rm ${INPUT}_tmp.pdf
-rm -r tmp
